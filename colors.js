fca209d49cb73908132b727078880fbeccf9f873
KOGR <66009682+egor1221@users.noreply.github.com>
2023-04-17 19:59:05 +0300

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,26 +1,3 @@
-// function randomColor(){
-//     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-//     let hexText = document.getElementById('color')
-//     let hexOne = ''
-
-//     for (let i = 0; i < 6; i++) {
-//         let element = Math.floor(Math.random() * hex.length);
-//         hexOne += hex[element]
-//     }
-
-//     let body = document.getElementsByTagName('body')[0].style.background = '#' + hexOne
-//     hexText.innerHTML = hexOne
-
-// }
-
-// let button = document.getElementsByTagName('button')
-
-// button.onclick = randomColor;
-
-// console.log(randomColor())
-
-
-
 document.getElementById('button').addEventListener('click', () => {
     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     let hexText = document.getElementById('color')
